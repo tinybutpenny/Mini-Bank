ab648ab5e455cdcf359a3e2508094b88542686e8
DUONG VAN THANG <taothang65@gmail.com>
2025-08-28 21:46:29 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
 #include "chung.h"
 #include "nhap.h"
 #include "xuat.h"
+int main() {
+
+    char tieptuc;
+    do {
+        std::cout << " Bạn có muốn tiếp tục không\n";
+        chonthoat();
+    } while (tieptuc == 'Y');
+    return 0;
+}
