ba167f03c230c68155958b5557d8b370a8d5d6f0
DUONG VAN THANG <taothang65@gmail.com>
2025-09-29 09:24:54 +0700

    up

--- src/chung/chung.cpp
+++ src/chung/chung.cpp
@@ -1,7 +1,7 @@
 #include "chung.h"
 
 std::string ten[1000]= {"duonffjhgffgghffgag"};
-std::string matkhau[1000] = {"thggfdgfdgvgfgfg20"};
+std::string matkhau[1000] = {"thggfdgfdggfdsgfdsvgfgfg20"};
 std::string mapin[1000] = {"879"};
 int sokhach = 0;
 
